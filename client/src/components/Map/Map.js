9b0e4c296a10d8b4f30f5342e3854d9367818050
unknown <tuankiet091002@gmail.com>
2022-11-23 10:54:43 +0700

    'add gitignore'

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -16,7 +16,7 @@ const Map = () => {
     // Important! Always set the container height explicitly
     <div style={{ height: '100vh', width: '100%' }}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: "AIzaSyBogcbadurzvTdLzTfIKaafyaBdzB8ILzM" }}
+        bootstrapURLKeys={{ key: "" }}
         yesIWantToUseGoogleMapApiInternals
         defaultCenter={defaultProps.center}
         defaultZoom={defaultProps.zoom}
